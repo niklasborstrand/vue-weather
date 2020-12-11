63a9f06dddc2b9f7cf5fe07205908081619d99e6
niklasborstrand <niklas@pinyagroup.com>
2020-12-11 15:49:44 +0100

    gh-pages

---
+++ precache-manifest.2aaacc52b59236acf67d4afda09aad36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cdddad954da0b1f621",
+    "url": "/vue-weather/css/app.3c5b7d43.css"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/vue-weather/img/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/vue-weather/img/warm-bg.635ed87d.jpg"
+  },
+  {
+    "revision": "1bcc51438c053b1793045076c624b330",
+    "url": "/vue-weather/index.html"
+  },
+  {
+    "revision": "e0cdddad954da0b1f621",
+    "url": "/vue-weather/js/app.d0465e71.js"
+  },
+  {
+    "revision": "beb87d205b75f32741af",
+    "url": "/vue-weather/js/chunk-vendors.91757a84.js"
+  },
+  {
+    "revision": "abb7651b3fb33cbb939069d3e6d6a7cc",
+    "url": "/vue-weather/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-weather/robots.txt"
+  }
+]);
